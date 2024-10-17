fb501d060cae1722a2a90211b96840b01122aa06
Danyi <99331148+DnyKristof@users.noreply.github.com>
2024-10-17 17:29:09 +0200

    init project (#2)

---
+++ DatabaseRelated/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using evoWatch.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace evoWatch.DatabaseRelated
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Services/Implementations/UserService.cs
@@ -0,0 +1,37 @@
+﻿using evoWatch.DatabaseRelated;
+using evoWatch.Models;
+using evoWatch.Models.DTO;
+
+namespace evoWatch.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly DatabaseContext _context;
+
+        public UserService(DatabaseContext context)
+        {
+               _context = context;
+        }
+        public void addUser(UserDTO user)
+        {
+            var result = new User()
+            {
+                Email = user.Email,
+                NormalName = user.NormalName,
+                Nickname = user.Nickname,
+                ImageUrl = user.ImageUrl,
+                PasswordHash = user.Password,
+                PasswordSalt = new byte[] { 1, 2, 3 }
+            };
+            _context.Users.Add(result);
+            _context.SaveChanges();
+        }
+
+        public List<User> getUsers()
+        {
+            var result = _context.Users.ToList();
+
+            return result;
+        }
+    }
+}
