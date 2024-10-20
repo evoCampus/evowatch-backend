596c1459d4312f7bd5fbbea94b3b9f0a216ab92a
Puskás Marcell <puskasmarcell05@gmail.com>
2024-10-20 12:19:02 +0200

    Implemented hashing and verifying the password

---
+++ Services/IHashService.cs
@@ -0,0 +1,11 @@
+﻿using static evoWatch.Models.Hash;
+
+namespace evoWatch.Services
+{
+    public interface IHashService
+    {
+        HashResult HashPassword(string password);
+
+        bool VerifyPassword(string password, string hash, byte[] salt);
+    }
+}
