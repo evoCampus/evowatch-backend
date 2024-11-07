b4e2350697a552ef3709746ef4e2d9266aa40f21
Pázmán András <139883125+andraspazman@users.noreply.github.com>
2024-11-07 17:47:54 +0100

    aa_test
    aa

---
+++ evoWatch.Database/Models/MoviesAndEpisodes.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace evoWatch.Database.Models
+{
+    public class MoviesAndEpisodes
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public required string Genre { get; set; }
+        public required DateTime ReleaseYear { get; set; }
+        public string Description { get; set; }
+        public required string Language { get; set; }
+        public required string Awards { get; set; }
+
+        public virtual Seasons Season { get; set; }
+
+        public virtual ProductionCompany ProductionCompany { get; set; }
+    }
+}
