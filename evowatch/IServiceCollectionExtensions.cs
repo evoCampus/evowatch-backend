ca0327522284c575ae169933c7c34a5b2db15e23
Puskás Marcell <puskasmarcell05@gmail.com>
2024-11-20 17:35:28 +0100

    Moved AddControllers, AddEndpointsApiExplorer and AddSwaggerGen back to Program.cs

--- evowatch/IServiceCollectionExtensions.cs
+++ evowatch/IServiceCollectionExtensions.cs
@@ -1,4 +1,3 @@
-using System.Reflection;
 using evoWatch.Services;
 using evoWatch.Services.Implementations;
 
@@ -8,13 +7,6 @@ namespace evoWatch
     {
         public static IServiceCollection AddEvoWatch(this IServiceCollection services)
         {
-            services.AddControllers();
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen(c => {
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
-            });
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IHashService, HashService>();
 
--- evowatch/Program.cs
+++ evowatch/Program.cs
@@ -1,7 +1,16 @@
+using System.Reflection;
 using evoWatch;
 using evoWatch.Database;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => {
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddEvoWatch();
 builder.Services.AddEvoWatchDatabase();
