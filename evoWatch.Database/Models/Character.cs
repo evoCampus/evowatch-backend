6471c18e4a9af9178845b8fa33bfdc663c977274
Pázmán András <139883125+andraspazman@users.noreply.github.com>
2024-12-09 22:38:42 +0100

    create annotations

--- evoWatch.Database/Models/Character.cs
+++ evoWatch.Database/Models/Character.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,17 +10,31 @@ namespace evoWatch.Database.Models
 {
     public class Character
     {
+        [Key]
         public Guid Id { get; set; }
+
+        [Required] 
+        [MaxLength(30, ErrorMessage = "CharacterName cannot be longer than 30 characters.")]
         public string CharacterName { get; set; }
+
+        [Required] 
+        [MaxLength(20, ErrorMessage = "Role cannot be longer than 20 characters.")]
         public string Role { get; set; }
+
+        [MaxLength(30, ErrorMessage = "NickName cannot be longer than 30 characters.")]
         public string NickName { get; set; }
 
-        public required Guid EpisodesId { get; set; }
-        public Episode Episodes { get; set; }
+        [Required]
+        public required Guid EpisodeId { get; set; }
 
+        [ForeignKey(nameof(EpisodeId))]
+        public Episode Episode { get; set; }
+
+        [Required]
         public required Guid PersonId { get; set; }
-        public Person People { get; set; }
 
+        [ForeignKey(nameof(PersonId))]
+        public Person Person { get; set; }
 
     }
 }
