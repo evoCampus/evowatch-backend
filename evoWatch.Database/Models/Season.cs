870369dcfde5d617750ad513af3dd428b8ab5f22
Pázmán András <139883125+andraspazman@users.noreply.github.com>
2024-12-09 22:38:42 +0100

    create annotations

--- evoWatch.Database/Models/Season.cs
+++ evoWatch.Database/Models/Season.cs
@@ -1,16 +1,28 @@
-﻿using System.Text.Json;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
 
 namespace evoWatch.Database.Models
 {
     public class Season
     {
+        [Key] 
         public Guid Id { get; set; }
+
+        [Required]
         public required DateTime ReleaseYear { get; set; }
-        public  int SeasonCount { get; set; }
-        public  int EpisodeCount { get; set; }
-       
-        public Guid SeriesId { get; set; } 
-        public virtual Series Series { get; set; }  
+
+        [Range(1, int.MaxValue, ErrorMessage = "SeasonCount must be greater than or equal to 1.")]
+        public int SeasonCount { get; set; } 
+
+        [Range(1, int.MaxValue, ErrorMessage = "EpisodeCount must be greater than or equal to 1.")]
+        public int EpisodeCount { get; set; }  
+
+        [Required]  
+        public Guid SeriesId { get; set; }
+
+        [ForeignKey(nameof(SeriesId))] 
+        public virtual Series Series { get; set; } 
 
         public virtual ICollection<Episode> Episodes { get; set; }
     }
