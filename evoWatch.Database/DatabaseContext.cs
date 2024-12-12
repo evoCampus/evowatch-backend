dfb4b38f1814beeeb0c089539b86e4482135e600
Pázmán András <139883125+andraspazman@users.noreply.github.com>
2024-12-12 17:25:48 +0100

    create documentation comments

--- evoWatch.Database/DatabaseContext.cs
+++ evoWatch.Database/DatabaseContext.cs
@@ -16,59 +16,12 @@ namespace evoWatch.Database
         public DbSet<Series> Series { get; set; }
 
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Series>()
-               .HasMany(x => x.Seasons)
-               .WithOne(x => x.Series)
-               .HasForeignKey(x => x.SeriesId);
-
-            modelBuilder.Entity<Season>()
-               .HasMany(x => x.Episodes)
-               .WithOne(x => x.Seasons)
-               .HasForeignKey(x => x.SeasonId);
-
-            modelBuilder.Entity<ProductionCompany>()
-                .HasMany(x => x.Episodes)
-                .WithOne(x => x.ProductionCompanies)
-                .HasForeignKey(x => x.ProductionCompanyId);
-
-            modelBuilder.Entity<Episode>()
-               .HasMany(x => x.Characters)
-               .WithOne(x => x.Episodes)
-               .HasForeignKey(x => x.EpisodesId);
-
-            modelBuilder.Entity<Episode>()
-               .HasMany(x => x.PersonEpisodes)
-               .WithOne(x => x.Episodes)
-               .HasForeignKey(x => x.EpisodesId);
-
-            modelBuilder.Entity<Person>()
-               .HasMany(x => x.Characters)
-               .WithOne(x => x.People)
-               .HasForeignKey(x => x.PersonId);
-
-            modelBuilder.Entity<Person>()
-              .HasMany(x => x.PeopleEpisodes)
-              .WithOne(x => x.People)
-              .HasForeignKey(x => x.PersoniD);
-
-            modelBuilder.Entity<PersonEpisode>()
-                .HasKey(x => new { x.PersoniD, x.EpisodesId });
-
-
-
-
-        }
-
 
 
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {
 
         }
-
-       
 
 
     }
--- evowatch/Controllers/SeriesController.cs
+++ evowatch/Controllers/SeriesController.cs
@@ -19,14 +19,29 @@ namespace evoWatch.Controllers
             _seriesService = seriesService;
         }
 
-        [HttpGet]
+        /// <summary>
+        ///  List of all series.
+        /// </summary>
+        /// <response code="200">The list of series was successfully listed.</response>
+        [HttpGet(Name = nameof(GetSeries))]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(IEnumerable<SeriesDTO>), StatusCodes.Status200OK)]
         public async Task<IActionResult> GetSeries()
         {
             var result = await _seriesService.GetSeriesAsync();
             return Ok(result);
         }
 
-        [HttpGet("{id:guid}")]
+        /// <summary>
+        /// List a series by id.
+        /// </summary>
+        /// <param name="id">The unique identifier of the series.</param>
+        /// <response code="200">The series was successfully retrieved.</response>
+        /// <response code="404">The series was not found.</response>
+        [HttpGet("{id:guid}", Name = nameof(GetSeriesById))]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(SeriesDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetSeriesById(Guid id)
         {
             try
@@ -36,19 +51,35 @@ namespace evoWatch.Controllers
             }
             catch (SeriesNotFoundException)
             {
-                return NotFound("series not found");
+                return NotFound("Series not found");
             }
         }
 
+        /// <summary>
+        /// Adds a new series.
+        /// </summary>
+        /// <param name="series">The series data to add.</param>
+        /// <response code="200">The series was successfully added.</response>
         [HttpPost(Name = nameof(AddSeries))]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(SeriesDTO), StatusCodes.Status200OK)]
         public async Task<IActionResult> AddSeries([FromBody] SeriesDTO series)
         {
             var result = await _seriesService.AddSeriesAsync(series);
             return Ok(result);
         }
 
-        [HttpPut("{id:guid}")]
+        /// <summary>
+        /// Updates an existing series id.
+        /// </summary>
+        /// <param name="id">The unique identifier of the series to update.</param>
+        /// <param name="series">The updated series data.</param>
+        /// <response code="200">The series was successfully updated.</response>
+        /// <response code="404">The series was not found.</response>
+        [HttpPut("{id:guid}", Name = nameof(UpdateSeries))]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(SeriesDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> UpdateSeries(Guid id, [FromBody] SeriesDTO series)
         {
             try
@@ -58,20 +89,31 @@ namespace evoWatch.Controllers
             }
             catch (SeriesNotFoundException)
             {
-                return NotFound("series not found");
+                return NotFound("Series not found");
             }
         }
 
-        [HttpDelete("{id:guid}")]
+        /// <summary>
+        /// Deletes a series id.
+        /// </summary>
+        /// <param name="id">The unique identifier of the series to delete.</param>
+        /// <response code="200">The series was successfully deleted.</response>
+        /// <response code="404">The series was not found.</response>
+        /// <response code="500">Failed to delete the series.</response>
+        [HttpDelete("{id:guid}", Name = nameof(DeleteSeriesAsync))]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> DeleteSeriesAsync(Guid id)
         {
             try
             {
-                var result = await _seriesService.DeleteSeriesAsync(id);             
+                var result = await _seriesService.DeleteSeriesAsync(id);
 
                 if (!result)
                 {
-                    return Problem("Failed to delete");
+                    return Problem("Failed to delete", null, StatusCodes.Status500InternalServerError);
                 }
                 else
                 {
@@ -80,7 +122,7 @@ namespace evoWatch.Controllers
             }
             catch (SeriesNotFoundException)
             {
-                return NotFound("series not found");
+                return NotFound("Series not found");
             }
         }
 
