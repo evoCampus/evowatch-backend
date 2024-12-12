cd82fe9c8e4d188dc99d7b699565d1016c89ff4e
Pázmán András <139883125+andraspazman@users.noreply.github.com>
2024-12-12 17:28:38 +0100

    aa_test
    aa

---
+++ evoWatch.Database/Models/PersonMoviesConnectionTable.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+
+namespace evoWatch.Database.Models
+{
+    internal class PersonMoviesConnectionTable
+    {
+        public virtual MoviesAndEpisodes Movies { get; set; }
+        public virtual Person Person { get; set; }
+    }
+}
--- evoWatch.Database/Models/Series.cs
+++ evoWatch.Database/Models/Series.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
 namespace evoWatch.Database.Models
