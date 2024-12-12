79744b03e3df67b08cb1c41bd1da975895dcae1b
Pázmán András <139883125+andraspazman@users.noreply.github.com>
2024-12-12 17:25:48 +0100

    Series services and controller  creating

---
+++ evoWatch.Database/Repositories/ISeriesRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using evoWatch.Database.Models;
+
+namespace evoWatch.Database.Repositories
+{
+    public interface ISeriesRepository
+    {
+        Task AddSeriesAsync(Series series);
+        Task<List<Series>> GetSeriesAsync();
+        Task UpdateSeriesAsync(Series series);
+        Task DeleteSeriesAsync(Series series);
+        
+    }
+}
---
+++ evowatch/Services/ISeriesService.cs
@@ -0,0 +1,11 @@
+﻿using evoWatch.Database.Models;
+using evoWatch.Models.DTOs;
+
+namespace evoWatch.Services
+{
+    public interface ISeriesService
+    {
+        Task AddSeriesAsync(SeriesDTO series);
+        Task<List<Series>> GetSeriesAsync(); 
+    }
+}
