1de5b9133a3b944b858ad471f5cc29737c7047c5
bence <67009276+bqnce@users.noreply.github.com>
2025-01-12 18:12:14 +0100

    Requested changes #1

---
+++ evoWatch.Database/Migrations/20250112171121_UserAnnotations.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace evoWatch.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserAnnotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nickname",
+                table: "Users",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Users",
+                type: "nvarchar(2048)",
+                maxLength: 2048,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nickname",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldMaxLength: 16);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2048)",
+                oldMaxLength: 2048,
+                oldNullable: true);
+        }
+    }
+}
--- evoWatch.Database/Models/User.cs
+++ evoWatch.Database/Models/User.cs
@@ -1,37 +1,32 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
 namespace evoWatch.Database.Models
 {
+    [Index(nameof(Email), IsUnique = true)]
     public class User
     {
         [Required]
         [MaxLength(30, ErrorMessage = "NormalName cannot be longer than: 30")]
         public string NormalName { get; set; }
-
         [Required]
         [MaxLength(32, ErrorMessage = "Email cannot be longer than: 32")]
         public string Email { get; set; }
-
         [MaxLength(16, ErrorMessage = "Nickname cannot be longer than: 16")]
         public string Nickname { get; set; }
-
         [MaxLength(2048, ErrorMessage = "ImageUrl cannot be longer than: 2048")]
         public string? ImageUrl { get; set; }
-
+        [Key]
         public Guid Id { get; set; }
-
         [Required]
         public string PasswordHash { get; set; }
-
+        [Required]
         public byte[] PasswordSalt { get; set; }
-
         public bool IsActive { get; set; }
-
         public User()
         {
         }
-
         public User(string normalName, string email, string passwordHash, byte[] passwordSalt, bool isActive, string nickname, string imageUrl)
         {
             Id = Guid.NewGuid();
